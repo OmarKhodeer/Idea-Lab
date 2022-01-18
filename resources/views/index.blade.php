3cb5060ad411430c2c6a518a177c59d510618076
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-01-18 15:38:50 +0200

    Design Idea Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,4 +29,204 @@
         </div>
     </div> {{-- end filters --}}
 
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in">
+                        Vote
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random Title can go here.</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum praesentium libero omnis in, quibusdam eum
+                        reprehenderit? Maxime asperiores quia animi autem eos pariatur eligendi optio ut sit magni culpa fugit repellat id, soluta
+                        voluptates repellendus natus ullam accusantium necessitatibus inventore aliquam molestiae? Ipsum facilis magni voluptas quas
+                        mollitia ipsa illum tenetur. Incidunt alias eos dolor fugit magni ullam perferendis ducimus molestiae provident, et nulla cum
+                        eveniet? Dolores excepturi earum nam quae iste ex, dolor consequatur, laborum qui asperiores harum eaque temporibus. Similique
+                        provident eos dignissimos consequuntur omnis perferendis sint aliquid inventore totam eaque hic, odit, molestiae quae dolore
+                        assumenda!
+                    </div>
+
+                    <div class="flex items center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2">Open</div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+
+                                <ul
+                                    class="absolute w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 text-left ml-8 transition duration-150 ease-in">
+                                    <li>
+                                        <a href="" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">
+                                            Mark as Spam
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">
+                                            Delete Post
+                                        </a>
+                                    </li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> {{-- end idea-container --}}
+
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl text-blue">66</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-blue font-bold text-white text-xxs uppercase rounded-xl px-4 py-3 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in">
+                        Voted
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random Title can go here.</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum praesentium libero omnis in, quibusdam eum
+                        reprehenderit? Maxime asperiores quia animi autem eos pariatur eligendi optio ut sit magni culpa fugit repellat id, soluta
+                        voluptates repellendus natus ullam accusantium necessitatibus inventore aliquam molestiae? Ipsum facilis magni voluptas quas
+                        mollitia ipsa illum tenetur. Incidunt alias eos dolor fugit magni ullam perferendis ducimus molestiae provident, et nulla cum
+                        eveniet? Dolores excepturi earum nam quae iste ex, dolor consequatur, laborum qui asperiores harum eaque temporibus. Similique
+                        provident eos dignissimos consequuntur omnis perferendis sint aliquid inventore totam eaque hic, odit, molestiae quae dolore
+                        assumenda!
+                    </div>
+
+                    <div class="flex items center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-yellow text-xxs text-white text-center font-bold uppercase leading-none rounded-full w-28 h-7 px-4 py-2">
+                                In Progress
+                            </div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> {{-- end idea-container --}}
+
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">0</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in">
+                        Vote
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random Title can go here.</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum praesentium libero omnis in, quibusdam eum
+                        reprehenderit? Maxime asperiores quia animi autem eos pariatur eligendi optio ut sit magni culpa fugit repellat id, soluta
+                        voluptates repellendus natus ullam accusantium necessitatibus inventore aliquam molestiae? Ipsum facilis magni voluptas quas
+                        mollitia ipsa illum tenetur. Incidunt alias eos dolor fugit magni ullam perferendis ducimus molestiae provident, et nulla cum
+                        eveniet? Dolores excepturi earum nam quae iste ex, dolor consequatur, laborum qui asperiores harum eaque temporibus. Similique
+                        provident eos dignissimos consequuntur omnis perferendis sint aliquid inventore totam eaque hic, odit, molestiae quae dolore
+                        assumenda!
+                    </div>
+
+                    <div class="flex items center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-red text-xxs text-white text-center font-bold uppercase leading-none rounded-full w-28 h-7 px-4 py-2">
+                                Closed
+                            </div>
+
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> {{-- end idea-container --}}
+    </div> {{-- end ideas-container --}}
+
+
 </x-app-layout>
