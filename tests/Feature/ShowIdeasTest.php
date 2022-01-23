8bb204460eab15cc55baa8e3c71f5c9b4d2c67c8
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-01-23 14:00:53 +0200

    Test Showing Ideas

---
+++ tests/Feature/ShowIdeasTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Idea;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowIdeasTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function list_of_ideas_shows_on_main_page()
+    {
+        $idea1 = Idea::factory()->create([
+            'title' => 'my first idea',
+            'description' => 'description of my first idea'
+        ]);
+
+        $idea2 = Idea::factory()->create([
+            'title' => 'my second idea',
+            'description' => 'description of my second idea'
+        ]);
+
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($idea1->title);
+        $response->assertSee($idea1->description);
+        $response->assertSee($idea2->title);
+        $response->assertSee($idea2->description);
+    }
+
+    /** @test */
+    public function single_idea_shows_correctly_in_the_show_page()
+    {
+        $idea = Idea::factory()->create([
+            'title' => 'my first idea',
+            'description' => 'description of my first idea'
+        ]);
+
+        $response = $this->get(route('idea.show', $idea));
+
+        $response->assertSuccessful();
+        $response->assertSee($idea->title);
+        $response->assertSee($idea->description);
+    }
+
+    /** @test */
+    public function ideas_pagintation_works()
+    {
+        Idea::factory(Idea::PAGINATION_COUNT + 1)->create();
+
+        $ideaOne = Idea::find(1);
+        $ideaOne->title = "My First Idea";
+        $ideaOne->save();
+
+        $ideaEleven = Idea::find(11);
+        $ideaEleven->title = "My Eleventh Idea";
+        $ideaEleven->save();
+
+        $response = $this->get('/');
+
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
+
+        $response = $this->get('/?page=2');
+
+        $response->assertSee($ideaEleven->title);
+        $response->assertDontSee($ideaOne->title);
+    }
+
+    /** @test */
+    public function same_idea_title_different_slugs()
+    {
+        $idea1 = Idea::factory()->create([
+            'title' => 'my idea',
+            'description' => 'my first description'
+        ]);
+
+        $idea2 = Idea::factory()->create([
+            'title' => 'my idea',
+            'description' => 'my second description'
+        ]);
+
+        $response = $this->get(route('idea.show', $idea1));
+
+        $response->assertSuccessful();
+        $this->assertTrue(request()->path() === 'ideas/my-idea');
+
+        $response = $this->get(route('idea.show', $idea2));
+
+        $response->assertSuccessful();
+        $this->assertTrue(request()->path() === 'ideas/my-idea-2');
+    }
+}
