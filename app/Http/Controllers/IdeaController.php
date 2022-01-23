f03e4c105da1bbf946392e1884e30c2c6a91f571
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-01-23 22:03:52 +0200

    Create Ideas

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -17,6 +17,7 @@ class IdeaController extends Controller
     {
         return view('idea.index', [
             'ideas' => Idea::with('user', 'category', 'status')
+                ->latest()
                 ->simplePaginate(Idea::PAGINATION_COUNT)
         ]);
     }
