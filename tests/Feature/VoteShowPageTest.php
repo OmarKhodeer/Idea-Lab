0939f443af7c2f11af82f0c8a53b76926a814844
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-02-05 22:33:21 +0200

    Check If Idea Voted For

--- tests/Feature/VoteShowPageTest.php
+++ tests/Feature/VoteShowPageTest.php
@@ -93,4 +93,35 @@ class VoteShowPageTest extends TestCase
         ])
             ->assertSet('votesCount', 5);
     }
+
+    /** @test */
+    public function user_who_logged_in_shows_voted_if_idea_already_voted_for()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(IdeaShow::class, [
+                'idea' => $idea,
+                'votesCount' => 5
+            ])
+            ->assertSet('hasVoted', true)
+            ->assertSee('Voted');
+    }
 }
