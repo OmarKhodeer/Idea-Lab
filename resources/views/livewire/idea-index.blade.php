85aeb96c3c123bdf419a1faec74987904d32487c
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-02-07 18:39:19 +0200

    Status Filters Test

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -51,10 +51,11 @@
                 </div>
 
                 <div x-data="{ isOpen: false }" class="flex items-center space-x-2 mt-4 md:mt-0">
-                    <div class="{{ $idea->status->classes }} text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2">
+                    <div class="{{ $idea->status->classes }} text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2"
+                        data-test="{{ $idea->status->name }}">
                         {{ $idea->status->name }}
                     </div>
-                    <button @click="isOpen = ! isOpen"
+                    <button @click=" isOpen=! isOpen"
                         class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 px-4 py-2 transition duration-150 ease-in">
                         <svg fill="currentColor" width="24" height="6">
                             <path
