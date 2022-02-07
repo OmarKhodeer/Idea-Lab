6d0777b85a3db35733b2ba4f63de6b1fb80c4527
OmarKhodeer <omarkhodeer.oh@gmail.com>
2022-02-07 19:46:14 +0200

    Category Filters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -9,33 +9,30 @@ use Livewire\Component;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
         // redirect to idea.index if previous route is idea.show
-        // if ($this->getPreviousRouteName() === 'idea.show') {
-        return redirect()->route('idea.index', [
-            'status' => $this->status
-        ]);
-        // }
+        if ($this->getPreviousRouteName() === 'idea.show') {
+            return redirect()->route('idea.index', [
+                'status' => $this->status
+            ]);
+        }
     }
 
     public function render()
@@ -45,6 +42,6 @@ class StatusFilters extends Component
 
     private function getPreviousRouteName()
     {
-        app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
+        return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
     }
 }
